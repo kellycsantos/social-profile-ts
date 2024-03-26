b5b7b068d1a452accc0c5c5190d5bd97eadbc10d
Kelly Ramos <kramos@fcc.org.br>
2024-03-26 15:02:51 -0300

    Primeira versão finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import './App.css';
+import Container from './components/Container';
+import List from './components/SocialLinks';
+import Icon from './assets/avatar-jessica.jpeg'
 
 function App() {
+
+  const socialInfo = (
+    [
+      {name: 'Github', link: '#'},
+      {name: 'Frontend Mentor', link: '#'},
+      {name: 'Linkedin', link: '#'},
+      {name: 'Twitter', link: '#'},
+      {name: 'Instagram', link: '#'},
+    ]
+  
+    )
   return (
     <div className="App">
-      <p>link profile</p>
+      <Container>
+        <img src={Icon} alt='Jessica' className="avatar" />
+       <span className='info'>
+        <h2>  Jessica Randall</h2>
+        <h4>London, United Kingdom</h4>
+        <p>"Front-end developer and avid reader."</p>
+       </span>
+        <List items={socialInfo} />
+      </Container>
     </div>
   );
 }
